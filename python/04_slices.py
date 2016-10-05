55383325531a0b490a4862925a7a59fe66b83f80
Carl Flippin <carlf@photocarl.org>
2016-10-05 15:36:02 -0700

    Add slice questions

---
+++ python/04_slices.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = a[-1]
+c = a[-3:]
+d = a[0:3]
+e = "This is only a test"
+f = e[1:5]
+g = a[:]
+
+assert a == False
+assert b == False
+assert c == False
+assert d == False
+assert e == False
+assert f == False
+assert g == False
